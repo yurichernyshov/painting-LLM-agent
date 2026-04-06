a55a1b66e14f1063082bcaea3816e5cf1315d515
yurichernyshov <ychernyshov@compute-vm-8-32-80-ssd-1770966437792.ru-central1.internal>
2026-04-06 20:15:18 +0000

    corrected utcnow to now

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def get_password_hash(password):
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.now() + (expires_delta or timedelta(minutes=15))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
